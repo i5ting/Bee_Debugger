5f760a62f6a81530a6ab674a07af961e49ff9471
i5ting <i5ting@126.com>
2013-04-16 16:35:33 +0800

    Initial Commit

---
+++ Bee_DebuggerTests/Bee_DebuggerTests.h
@@ -0,0 +1,13 @@
+//
+//  Bee_DebuggerTests.h
+//  Bee_DebuggerTests
+//
+//  Created by sang on 4/16/13.
+//  Copyright (c) 2013 alfred sang. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Bee_DebuggerTests : SenTestCase
+
+@end
---
+++ Bee_DebuggerTests/Bee_DebuggerTests.m
@@ -0,0 +1,32 @@
+//
+//  Bee_DebuggerTests.m
+//  Bee_DebuggerTests
+//
+//  Created by sang on 4/16/13.
+//  Copyright (c) 2013 alfred sang. All rights reserved.
+//
+
+#import "Bee_DebuggerTests.h"
+
+@implementation Bee_DebuggerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Bee_DebuggerTests");
+}
+
+@end
